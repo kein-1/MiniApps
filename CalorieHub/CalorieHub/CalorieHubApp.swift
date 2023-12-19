d02cad5940578c90d71bcb87c3682d3b693f970c
Kein <tensaikev@gmail.com>
2023-12-19 00:05:28 -0500

    start

---
+++ CalorieHub/CalorieHub/CalorieHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieHubApp.swift
+//  CalorieHub
+//
+//  Created by Kein Li on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CalorieHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalorieHub/CalorieHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalorieHub
+//
+//  Created by Kein Li on 12/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
