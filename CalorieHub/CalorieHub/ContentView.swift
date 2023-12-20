ca75c23ccc9b67ee043fc475863a631b65d4f15d
Kein <tensaikev@gmail.com>
2023-12-20 00:24:20 -0500

    Incorporated HealthKit, worked on Onboarding View

--- CalorieHub/CalorieHub/ContentView.swift
+++ CalorieHub/CalorieHub/ContentView.swift
@@ -6,19 +6,43 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
+    
+    var healthStore : HealthStore
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            
+            VStack(alignment: .trailing, spacing: 20) {
+                Text("View 1")
+                Text("View 2")
+                Text("View 3")
+                
+                Spacer()
+            }
+            .padding()
+            .border(.blue)
+            .tabItem {
+                Label("random", systemImage: "figure.walk")
+            }
+            
+            Text("view1")
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            Text("view2")
+                .tabItem {
+                    Label("Personal", systemImage: "person")
+                }
+            
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    let healthStore = HealthStore()
+    return ContentView(healthStore: healthStore)
 }
---
+++ CalorieHub/CalorieHub/Model/FoodModel.swift
@@ -0,0 +1,24 @@
+//
+//  FoodModel.swift
+//  CalorieHub
+//
+//  Created by Kein Li on 12/19/23.
+//
+
+import Foundation
+import SwiftData
+
+// Food model : user tracks each food
+
+//@Model
+//class Food {
+//    var name : String
+//    var date : Date
+//    
+//    var calories : Double
+//    
+////    var protein : HKCorr
+//    
+//    
+//    
+//}
