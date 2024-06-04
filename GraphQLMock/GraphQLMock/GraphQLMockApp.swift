a663ea272b83efe4d2ea6ac72760484fb4a726bf
Kein <tensaikev@gmail.com>
2024-06-04 16:49:16 -0400

    GraphQLMock

---
+++ GraphQLMock/GraphQLMock/GraphQLMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLMockApp.swift
+//  GraphQLMock
+//
+//  Created by Kein Li on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
